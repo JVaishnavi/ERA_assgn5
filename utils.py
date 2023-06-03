8559db997385eed5fdfbd76e5b5c18af698ecb56
JVaishnavi <vaishnavijanakiraman1@gmail.com>
2023-06-03 08:56:14 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -7,7 +7,30 @@ Created on Sat Jun  3 08:00:51 2023
 """
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+cuda = torch.cuda.is_available()
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+from tqdm import tqdm
+
+
+    
+def plot_loss(train_losses, train_acc, test_losses, test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+    
+
