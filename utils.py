2028868eac358ead03cbef9559372cd6cac8a3ff
JVaishnavi <vaishnavijanakiraman1@gmail.com>
2023-06-03 08:09:04 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  3 08:00:51 2023
+
+@author: vaishnavijanakiraman
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
